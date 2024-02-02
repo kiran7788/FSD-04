f3b123784c573231f9743bd7e14ce62ba21b9da0
kiran <kk64956.k@gmail.com>
2024-02-02 17:26:43 +0530

    test commit

--- src/FSD1/basics/day2.java
+++ src/FSD1/basics/day2.java
@@ -1,10 +1,12 @@
 package FSD1.basics;
 
+import java.util.Scanner;
+
 public class day2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner
+		Scanner scanner = new Scanner
 	}
-
+jnfdskl
 }
